b1fc0a3a7bb4418e2c35cb54f6f1a65796c734c2
Yash Divekar <133482969+Yash-Divekar@users.noreply.github.com>
2025-03-21 04:05:23 +0530

    1

--- src/pages/Events Page/Events.tsx
+++ src/pages/Events Page/Events.tsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react';
 import { motion, useInView } from 'framer-motion';
-import { LazyLoadImage } from "react-lazy-load-image-component";
+//import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 
 
